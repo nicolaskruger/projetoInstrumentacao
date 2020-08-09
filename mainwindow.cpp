767cbd0a1cfac08f5102572fa598eb0f38f49853
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-08-09 11:11:53 -0300

    01

---
+++ mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QString>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString str = ui->plainTextEdit->toPlainText();
+    ui->plainTextEdit->setPlainText(str+"foi\n");
+}
